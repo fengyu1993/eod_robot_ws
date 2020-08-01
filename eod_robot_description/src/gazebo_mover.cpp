9ae278a4e233058af0b9a7fce2dee012ee679b92
fengyu1993 <3120185244@bit.edu.cn>
2020-08-01 12:55:26 +0800

    eod_robot_dependlib/roboticsgroup_gazebo_plugins

--- eod_robot_description/src/gazebo_mover.cpp
+++ eod_robot_description/src/gazebo_mover.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "gazebo_mover");
 
   ros::NodeHandle n_R1, n_R2, n_R3, n_R4, n_R5, n_R6, n_L1, n_L2, n_L3, n_L4, n_L5, n_L6;
-  
+                                                                  
   ros::Publisher joint_R1_pub = n_R1.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R1_joint_position_controller/command", 10);
   ros::Publisher joint_R2_pub = n_R2.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R2_joint_position_controller/command", 10);
   ros::Publisher joint_R3_pub = n_R3.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R3_joint_position_controller/command", 10);
@@ -41,18 +41,20 @@ int main(int argc, char **argv)
     std_msgs::Float64 joint_R1_angle, joint_R2_angle, joint_R3_angle, joint_R4_angle, joint_R5_angle, joint_R6_angle,
                       joint_L1_angle, joint_L2_angle, joint_L3_angle, joint_L4_angle, joint_L5_angle, joint_L6_angle;
 
-    joint_R1_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_R2_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_R3_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_R4_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_R5_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_R6_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_L1_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_L2_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_L3_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_L4_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_L5_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
-    joint_L6_angle.data=sin(2*M_PI*0.1*elapsed)*(M_PI/2);
+    joint_R1_angle.data=2.5;
+    joint_R2_angle.data=3.14;
+    joint_R3_angle.data=0;
+    joint_R4_angle.data=0;
+    joint_R5_angle.data=0;
+    joint_R6_angle.data=0;
+    joint_L1_angle.data=0;
+    joint_L2_angle.data=0;
+    joint_L3_angle.data=0;
+    joint_L4_angle.data=0;
+    joint_L5_angle.data=0;
+    joint_L6_angle.data=0;
+
+    // std::cout << "joint_R1_angle = " << joint_R1_angle.data << std::endl;
 
 	joint_R1_pub.publish(joint_R1_angle);
 	joint_R2_pub.publish(joint_R2_angle);
