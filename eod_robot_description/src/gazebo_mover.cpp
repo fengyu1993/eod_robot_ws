170ad55f431f7a6705329dc036b60c6dacb914d6
fengyu1993 <3120185244@bit.edu.cn>
2020-08-03 20:37:23 +0800

    gazebo

--- eod_robot_description/src/gazebo_mover.cpp
+++ eod_robot_description/src/gazebo_mover.cpp
@@ -1,18 +1,32 @@
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
+#include <sensor_msgs/JointState.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <math.h>
 
+double arm_right_joint[6], arm_left_joint[6], gripper_joint;
+
+void joint_states_Callback(const sensor_msgs::JointState::ConstPtr& msg)
+{
+    for(int i = 0; i < 6; i++)
+      arm_right_joint[i] = msg->position[i];
+
+    gripper_joint = msg->position[6];
+
+    for(int i = 0; i < 6; i++)
+      arm_left_joint[i] = msg->position[i+14];
+}
+
 int main(int argc, char **argv)
 {
-	
-  
   ros::init(argc, argv, "gazebo_mover");
 
   ros::NodeHandle n_R1, n_R2, n_R3, n_R4, n_R5, n_R6, 
                   n_L1, n_L2, n_L3, n_L4, n_L5, n_L6,
-                  n_gripper;
-                                                                  
+                  n_gripper, n_sub;
+
+  ros::Subscriber joint_pub = n_sub.subscribe("/joint_states", 10, joint_states_Callback);
+
   ros::Publisher joint_R1_pub = n_R1.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R1_joint_position_controller/command", 10);
   ros::Publisher joint_R2_pub = n_R2.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R2_joint_position_controller/command", 10);
   ros::Publisher joint_R3_pub = n_R3.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R3_joint_position_controller/command", 10);
@@ -41,23 +55,23 @@ int main(int argc, char **argv)
 
   while (ros::ok())	
   {  
-    joint_R1_angle.data = 1.5708;
-    joint_R2_angle.data = -0.4363;
-    joint_R3_angle.data = 0.0000;
-    joint_R4_angle.data = -1.5708;
-    joint_R5_angle.data = 1.5708;
-    joint_R6_angle.data = -1.5708;
+    joint_R1_angle.data = arm_right_joint[0];
+    joint_R2_angle.data = arm_right_joint[1];
+    joint_R3_angle.data = arm_right_joint[2];
+    joint_R4_angle.data = arm_right_joint[3];
+    joint_R5_angle.data = arm_right_joint[4];
+    joint_R6_angle.data = arm_right_joint[5];
 
-    joint_L1_angle.data = -1.5708;
-    joint_L2_angle.data = -2.7053;
-    joint_L3_angle.data = 0.0000;
-    joint_L4_angle.data = -1.5708;
-    joint_L5_angle.data = -1.5708;
-    joint_L6_angle.data = -1.5708;
+    joint_L1_angle.data = arm_left_joint[0];
+    joint_L2_angle.data = arm_left_joint[1];
+    joint_L3_angle.data = arm_left_joint[2];
+    joint_L4_angle.data = arm_left_joint[3];
+    joint_L5_angle.data = arm_left_joint[4];
+    joint_L6_angle.data = arm_left_joint[5];
 
     trajectory_msgs::JointTrajectoryPoint points_n;
 
-    points_n.positions.push_back(0.00);
+    points_n.positions.push_back(gripper_joint);
     joint_traj_gripper.points[0] = points_n;
     joint_traj_gripper.points[0].time_from_start = ros::Duration(1.0);
     joint_traj_gripper.header.stamp = ros::Time::now();
@@ -78,6 +92,8 @@ int main(int argc, char **argv)
 
     joint_gripper_pub.publish(joint_traj_gripper);
     
+    ros::spinOnce();
+
     loop_rate.sleep();
  
   }
