a8424f602f42fd6ef645b85f20e274ef785aaceb
fengyu1993 <3120185244@bit.edu.cn>
2020-08-07 15:21:39 +0800

    vehicle

--- eod_robot_description/src/gazebo_mover.cpp
+++ eod_robot_description/src/gazebo_mover.cpp
@@ -1,23 +1,20 @@
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
+#include <geometry_msgs/Twist.h>
 #include <sensor_msgs/JointState.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <math.h>
 
 double arm_right_joint[6], arm_left_joint[6], gripper_joint, 
-       vehicle_left_front, vehicle_left_back, vehicle_right_front, vehicle_right_back;
+       vehicle_linear, vehicle_anglar;
 
 void joint_states_Callback(const sensor_msgs::JointState::ConstPtr& msg)
 {
   for(int i = 0; i < msg->position.size(); i++){
-    if (msg->name[i] == "left_front_wheel_joint")
-      vehicle_left_front = msg->position[i] * 10;
-    else if(msg->name[i] == "right_front_wheel_joint")
-      vehicle_right_front = msg->position[i] * 10;
-    else if (msg->name[i] == "right_back_wheel_joint")
-      vehicle_right_back = msg->position[i] * 10;
+    if (msg->name[i] == "right_back_wheel_joint")
+      vehicle_linear = msg->position[i];
     else if(msg->name[i] == "left_back_wheel_joint")
-      vehicle_left_back = msg->position[i] * 10;
+      vehicle_anglar = msg->position[i];
     else if(msg->name[i] == "Arm_R1_joint")
       arm_right_joint[0] = msg->position[i];
     else if(msg->name[i] == "Arm_R2_joint")
@@ -53,17 +50,14 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "gazebo_mover");
 
-  ros::NodeHandle n_LF, n_RF, n_LB, n_RB,
-                  n_R1, n_R2, n_R3, n_R4, n_R5, n_R6, 
+  ros::NodeHandle n_R1, n_R2, n_R3, n_R4, n_R5, n_R6, 
                   n_L1, n_L2, n_L3, n_L4, n_L5, n_L6,
-                  n_gripper, n_sub;
+                  n_gripper, n_sub,
+                  n_vel;
 
   ros::Subscriber joint_pub = n_sub.subscribe("/joint_states", 10, joint_states_Callback);
 
-  ros::Publisher joint_LF_pub = n_LF.advertise<std_msgs::Float64>("/eod_robot_description/left_front_velocity_controller/command", 1);
-  ros::Publisher joint_RF_pub = n_RF.advertise<std_msgs::Float64>("/eod_robot_description/right_front_velocity_controller/command", 1);
-  ros::Publisher joint_LB_pub = n_LB.advertise<std_msgs::Float64>("/eod_robot_description/left_back_velocity_controller/command", 1);
-  ros::Publisher joint_RB_pub = n_RB.advertise<std_msgs::Float64>("/eod_robot_description/right_back_velocity_controller/command", 1);
+  ros::Publisher vel_pub = n_vel.advertise<geometry_msgs::Twist>("/eod_robot_description/cmd_vel", 1);
 
   ros::Publisher joint_R1_pub = n_R1.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R1_joint_position_controller/command", 1);
   ros::Publisher joint_R2_pub = n_R2.advertise<std_msgs::Float64>("/eod_robot_description/Arm_R2_joint_position_controller/command", 1);
@@ -82,9 +76,10 @@ int main(int argc, char **argv)
   ros::Publisher joint_gripper_pub = n_gripper.advertise<trajectory_msgs::JointTrajectory>("/eod_robot_description/Gripper_finger1_joint_position_controller/command", 10);
 
   ros::Rate loop_rate(50);
-  
-  std_msgs::Float64 joint_LF_velocity, joint_RF_velocity, joint_LB_velocity, joint_RB_velocity, 
-                    joint_R1_angle, joint_R2_angle, joint_R3_angle, joint_R4_angle, joint_R5_angle, joint_R6_angle,
+
+  geometry_msgs::Twist vel;
+
+  std_msgs::Float64 joint_R1_angle, joint_R2_angle, joint_R3_angle, joint_R4_angle, joint_R5_angle, joint_R6_angle,
                     joint_L1_angle, joint_L2_angle, joint_L3_angle, joint_L4_angle, joint_L5_angle, joint_L6_angle;
   
   trajectory_msgs::JointTrajectory joint_traj_gripper;
@@ -96,10 +91,12 @@ int main(int argc, char **argv)
 
   while (ros::ok())	
   {  
-    joint_LF_velocity.data = vehicle_left_front;
-    joint_RF_velocity.data = vehicle_right_front;
-    joint_LB_velocity.data = vehicle_left_back;
-    joint_RB_velocity.data = vehicle_right_back;
+    vel.linear.x = vehicle_linear;
+    vel.linear.y = 0.0;
+    vel.linear.z = 0.0;
+    vel.angular.x = 0.0;
+    vel.angular.y = 0.0;
+    vel.angular.z = vehicle_anglar;
 
     joint_R1_angle.data = arm_right_joint[0];
     joint_R2_angle.data = arm_right_joint[1];
@@ -122,10 +119,7 @@ int main(int argc, char **argv)
     joint_traj_gripper.points[0].time_from_start = ros::Duration(1.0);
     joint_traj_gripper.header.stamp = ros::Time::now();
 
-    joint_LF_pub.publish(joint_LF_velocity);
-    joint_RF_pub.publish(joint_RF_velocity);
-    joint_LB_pub.publish(joint_LB_velocity);
-    joint_RB_pub.publish(joint_RB_velocity);
+    vel_pub.publish(vel);
 
     joint_R1_pub.publish(joint_R1_angle);
     joint_R2_pub.publish(joint_R2_angle);
