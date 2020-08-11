2525136b0552d7039f579e0c751ee421e6b05ba8
fengyu1993 <3120185244@bit.edu.cn>
2020-08-11 20:43:26 +0800

    camera

--- eod_robot_library/src/test.cpp
+++ eod_robot_library/src/test.cpp
@@ -7,32 +7,32 @@ using namespace std;
 
 int main(int argc, char** argv)
 { 
-    MatrixXd R_Slist(6,6);
-    R_Slist << 0, 0, 0, 0, 0, 0, 0,1.0000,1.0000,1.0000, 0, 1.0000, 1.0000, 0, 0, 0, -1.0000, 0, 0,-0.0805,-0.0805,-0.0805,-0.1110,0.0296, 0, 0, 0, 0,0.8091, 0, 0, 0,0.4247,0.8091, 0,0.8091;
+    // MatrixXd R_Slist(6,6);
+    // R_Slist << 0, 0, 0, 0, 0, 0, 0,1.0000,1.0000,1.0000, 0, 1.0000, 1.0000, 0, 0, 0, -1.0000, 0, 0,-0.0805,-0.0805,-0.0805,-0.1110,0.0296, 0, 0, 0, 0,0.8091, 0, 0, 0,0.4247,0.8091, 0,0.8091;
 
-    Matrix4d R_M(4,4);
-    R_M << -1.0000, 0, 0, 0.8091, 0, 0, 1.0000, 0.3790, 0, 1.0000, 0, -0.0296, 0, 0, 0, 1.0000;
+    // Matrix4d R_M(4,4);
+    // R_M << -1.0000, 0, 0, 0.8091, 0, 0, 1.0000, 0.3790, 0, 1.0000, 0, -0.0296, 0, 0, 0, 1.0000;
 
-    Matrix4d T_base_R(4,4);
-    T_base_R <<  1.0000, 0,  0, -0.0500, 0, 0.4229, -0.9062, -0.1373, 0, 0.9062, 0.4229, -0.0906, 0, 0, 0, 1.0000;
+    // Matrix4d T_base_R(4,4);
+    // T_base_R <<  1.0000, 0,  0, -0.0500, 0, 0.4229, -0.9062, -0.1373, 0, 0.9062, 0.4229, -0.0906, 0, 0, 0, 1.0000;
     
-    Matrix4d T(4,4);
-    T << 0.0795304,  -0.986297,  -0.144544,   0.767886,-0.99676, -0.0769389, -0.0234398,  -0.233008, 0.0119975,    0.14594,  -0.989221,  -0.216464,0, 0, 0, 1;
+    // Matrix4d T(4,4);
+    // T << 0.0795304,  -0.986297,  -0.144544,   0.767886,-0.99676, -0.0769389, -0.0234398,  -0.233008, 0.0119975,    0.14594,  -0.989221,  -0.216464,0, 0, 0, 1;
     
-    VectorXd thetalist0(6), thetalist(6);
-    thetalist0 << 0.0022273, -0.310563,  0.570518, -1.47717, -2.70454, 0.243157;
+    // VectorXd thetalist0(6), thetalist(6);
+    // thetalist0 << 0.0022273, -0.310563,  0.570518, -1.47717, -2.70454, 0.243157;
 
 
-    double eomg = 0.01, ev = 0.001;
+    // double eomg = 0.01, ev = 0.001;
 
-    bool suc = eod_robot_IKinSpace(R_Slist, R_M, T, thetalist0, 0.01, 0.001, thetalist, T_base_R);
+    // bool suc = eod_robot_IKinSpace(R_Slist, R_M, T, thetalist0, 0.01, 0.001, thetalist, T_base_R);
 
-    Matrix4d T_check = eod_robot_FKinSpace(R_M, R_Slist, thetalist, T_base_R);
+    // Matrix4d T_check = eod_robot_FKinSpace(R_M, R_Slist, thetalist, T_base_R);
 
-    cout << "suc = " << suc << endl;
-    cout << "thetalist0 = " << thetalist0 << endl;
-    cout << "thetalist = " << thetalist << endl;
-    cout << "T_error: " << T_check - T << endl;
+    // cout << "suc = " << suc << endl;
+    // cout << "thetalist0 = " << thetalist0 << endl;
+    // cout << "thetalist = " << thetalist << endl;
+    // cout << "T_error: " << T_check - T << endl;
 
     // Matrix3d R(3,3);
     // R << 1.00002,  6.32013e-06,  2.22446e-06, 6.53443e-06, 1,  -2.28938e-06, -3.48345e-07,  -3.59979e-06, 1.00002;
@@ -75,4 +75,11 @@ int main(int argc, char** argv)
     // Eigen::Matrix3d R3 = euler2RotationMatrix(rpy2[2], rpy2[1], rpy2[0]);
     // cout << "R 3: " << R3 << endl;
 
+    Eigen::Quaterniond q;
+    q.x() = 0; q.y() = 0; q.z() = 0; q.w() = 0; 
+    
+    Eigen::Matrix3d R = Quaternion2RotationMatrix(q.x(), q.y(), q.z(), q.w());
+    Eigen::Quaterniond q2 = rotationMatrix2Quaterniond(R);
+    cout << "R = " << R << endl;
+    cout << "q2 = " << q2.x() << ", " << q2.y() << ", " << q2.z() << ", " <<  q2.w() << endl;
 }
