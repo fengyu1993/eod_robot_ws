460d4adbf1b632b954cd9527cb95bdab52632676
CYH <you@example.com>
2020-09-28 21:31:32 +0800

    move_learn

--- eod_robot_moveit_learn/src/move_group_interface_tutorial.cpp
+++ eod_robot_moveit_learn/src/move_group_interface_tutorial.cpp
@@ -15,7 +15,14 @@ int main(int argc, char** argv)
     ros::NodeHandle node_handle;
     ros::AsyncSpinner spinner(1);
     spinner.start();
+    
+    static const std::string PLANNING_GROUP = "right_arm";
 
+    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+    const robot_state::JointModelGroup* joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
 
 }
