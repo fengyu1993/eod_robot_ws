b0e811b15584a687eef2e7e67d0231801278cace
CYH <you@example.com>
2020-12-14 17:17:29 +0800

    ros_controller

---
+++ eod_robot_moveit_learn/doc/perception_pipeline/src/bag_publisher_maintain_time.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/PointCloud2.h>
+
+int main(int argc, char** argv)
+{
+    
+}
---
+++ eod_robot_moveit_learn/doc/perception_pipeline/src/cylinder_segment.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/CollisionObject.h>
+
+int main(int argc, char** argv)
+{
+
+}
